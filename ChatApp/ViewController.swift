c067403e5b365d2a913dfe8f4fd22f85010bc48b
Mertcncifter <Mertcncifter@gmail.com>
2023-04-15 00:41:19 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by mert can çifter on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
